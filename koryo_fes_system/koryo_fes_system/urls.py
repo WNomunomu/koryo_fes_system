ea07c33edde2ce5a8c55c8f2d24d668574f772e7
tangent-theta <tomotomo0412mogi@gmail.com>
2022-03-10 22:58:29 +0900

    set api

--- koryo_fes_system/koryo_fes_system/urls.py
+++ koryo_fes_system/koryo_fes_system/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import routers
+
+router = routers.DefaultRouter()
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('room_access_system.urls')),
+    path('api/', include(router.urls)),
 ]
