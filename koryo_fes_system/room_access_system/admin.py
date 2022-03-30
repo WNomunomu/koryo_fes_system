3da2db79b2df61a809cc515e709d6415adb334ee
WNomunomu <kohsei.nmr@gmail.com>
2022-03-31 04:05:15 +0900

    管理画面のモデルの表示のテーブル化

--- koryo_fes_system/room_access_system/admin.py
+++ koryo_fes_system/room_access_system/admin.py
@@ -2,4 +2,7 @@ from django.contrib import admin
 from .models import roomAccessData
 
 # Register your models here.
-admin.site.register(roomAccessData)
+@admin.register(roomAccessData)
+class RoomAccessDataAdmin(admin.ModelAdmin):
+    model = roomAccessData
+    list_display = ["room_name", "user_num", "entered_or_left", "entered_or_left_time"]
